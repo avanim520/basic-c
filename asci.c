b63b5a20af1499af8ddf73214f521459ca5ab44b
avanim520 <53889972+avanim520@users.noreply.github.com>
2019-10-16 10:25:13 +0530

    Add files via upload

---
+++ asci.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("enter the character");
+    scanf("%c",&c);
+    printf("%d",c);
+}
---
+++ data.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+    clrscr();
+    int num1,num2;
+    float f;
+    char c;
+    printf("enter two numbers");
+    scanf("%d%d\n",&num1,&num2);
+    printf("%d%d\n",num1,num2);
+    printf("enter the float value");
+    scanf("%f\n",&f);
+    printf("%f\n",f);
+    printf("enter the chracter\n ");
+    scanf("%c\n",&c);
+    printf("%c\n",c);
